675745374fdd5fb90c07f91e29cafe87c827f99c
Anurag Elwyn <anuragelwyn@gmail.com>
2022-11-01 01:29:52 +0530

    install firebase and react-toastify and sign up the user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import {useState} from 'react'
-import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai'
+import React from 'react';
+import {useState} from 'react';
+import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {db} from "../firebase";
+import {doc, serverTimestamp, setDoc } from 'firebase/firestore';
+import {useNavigate} from "react-router-dom";
+import { toast } from 'react-toastify';
 
 function SignUp() {
     const [showPassword, setShowPassword] = useState(false);
@@ -12,11 +17,29 @@ function SignUp() {
         password: ""
     });
     const {name, email, password} = formData;
+    const navigate = useNavigate()
     function onChange(e) {
         setFormData((prevState)=>({
             ...prevState,
             [e.target.id]: e.target.value,
         }))
+    }
+    async function onSubmit(e) {
+      e.preventDefault()
+      try {
+        const auth = getAuth();
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        updateProfile(auth.currentUser, {displayName: name})
+        const user = userCredential.user;
+        const formDataCopy = {...formData};
+        delete formDataCopy.password;
+        formDataCopy.timestamp = serverTimestamp();
+        await setDoc(doc(db, "users", user.uid), formDataCopy);
+        // toast.success("Sign up was successful");
+        navigate("/");
+      } catch (error) {
+        toast.error("Something went wrong with the registration")
+      }
     }
   return (
     <section>
@@ -26,7 +49,7 @@ function SignUp() {
                 <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80" alt="key" className='w-full rounded-2xl'/>
             </div>
             <div>
-                <form className='w-full md:w-[67%] lg:ml-20'>
+                <form onSubmit={onSubmit} className='w-full md:w-[67%] lg:ml-20'>
                     <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type="text" id='name' value={name} onChange={onChange} placeholder="Full name" />
                     <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type="email" id='email' value={email} onChange={onChange} placeholder="Email address" />
                     <div className='relative mb-6'>
